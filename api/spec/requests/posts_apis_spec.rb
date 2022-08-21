ce564b8d06bc373d03bef8b98682525ffaf8732e
伊東和成 <itokazunari5375@icloud.com>
2022-08-21 21:01:00 +0900

    新規作成のテストを作成

--- api/spec/requests/posts_apis_spec.rb
+++ api/spec/requests/posts_apis_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'GET /posts' do
-  it '全ての投稿を取得' do
+  it '全ての記事を取得' do
     FactoryBot.create_list(:post, 10)
 
     get '/posts'
@@ -15,7 +15,7 @@ describe 'GET /posts' do
 end
 
 describe 'GET /posts/:id' do
-  it '特定のタスクを取得する' do
+  it '特定の記事を取得する' do
     post = create(:post, title: "サンプルタスク")
     get "/posts/#{post.id}"
     json = JSON.parse(response.body)
@@ -23,5 +23,15 @@ describe 'GET /posts/:id' do
     expect(response.status).to eq(200)
     # # 正しい数のデータが返されたか確認する。
     expect(json["title"]).to eq(post["title"])
+  end
+end
+
+describe 'Post /posts' do
+  it '新しい記事を作成する' do
+    valid_params = { title: "test" }
+
+    #データが作成されていることを確認
+    expect { post '/posts', params: valid_params }.to change(Post, :count).by(+1)
+    expect(response.status).to eq(201)
   end
 end
