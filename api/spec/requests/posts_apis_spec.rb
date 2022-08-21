e1b4c556a7131fcf689a303ca815a0c817a848ac
伊東和成 <itokazunari5375@icloud.com>
2022-08-21 20:59:08 +0900

    feat: 特定要素の取得テストを作成

--- api/spec/requests/posts_apis_spec.rb
+++ api/spec/requests/posts_apis_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'GET /tasks' do
+describe 'GET /posts' do
   it '全ての投稿を取得' do
     FactoryBot.create_list(:post, 10)
 
@@ -11,5 +11,17 @@ describe 'GET /tasks' do
     expect(response.status).to eq(200)
     # 正しい数のデータが返されたか確認する。
     expect(json.length).to eq(10)
+  end
+end
+
+describe 'GET /posts/:id' do
+  it '特定のタスクを取得する' do
+    post = create(:post, title: "サンプルタスク")
+    get "/posts/#{post.id}"
+    json = JSON.parse(response.body)
+    # # リクエスト成功を表す200が返ってきたか確認する。
+    expect(response.status).to eq(200)
+    # # 正しい数のデータが返されたか確認する。
+    expect(json["title"]).to eq(post["title"])
   end
 end
